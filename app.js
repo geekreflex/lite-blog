4e2012eb499ead326339c455ceff395a3e8275f6
jerry <jerrynwosu007@gmail.com>
2021-07-23 01:18:31 +0100

    add build path in app

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
+require("dotenv").config();
+const path = require("path");
 const express = require("express");
-const app = express();
 const cors = require(cors);
+
+const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+const __dirname = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/client/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running...");
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
